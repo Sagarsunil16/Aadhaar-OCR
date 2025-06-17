458b99041a099f7721648de5d3c6b9c1adceb6ee
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2025-06-17 13:51:58 +0530

    multer config done

---
+++ api/src/config/multer.ts
@@ -0,0 +1,34 @@
+import multer from "multer";
+import path from "path";
+import fs from 'fs'
+const uploadDir = path.join(__dirname,'../uploads')
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir)
+}
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,uploadDir)
+    },
+    filename:(req,file,cb)=>{
+        cb(null,`${file.filename}-${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage,
+    limits:{fileSize: 5 * 1024 * 1024 } , //5MB
+    fileFilter:(req,file,cb)=>{
+        const fileTypes =  / jpeg | jpg | png /
+        const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+        const mimetype =  fileTypes.test(file.mimetype)
+        if (extname && mimetype) {
+        return cb(null, true);
+    }
+        cb(new Error('Only JPEG and PNG images are allowed'));
+    }
+}).fields([
+  { name: 'frontImage', maxCount: 1 },
+  { name: 'backImage', maxCount: 1 },
+]);
+
+export default upload
